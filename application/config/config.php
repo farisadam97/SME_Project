d60f73a3e0be405d8a75399925c370df00b3a7ed
abcdbar <bobbyilham44@yahoo.com>
2018-08-08 13:49:30 +0700

    a
    a

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/SME/';
+$config['base_url'] = 'http://localhost/SME-project/';
 
 /*
 |--------------------------------------------------------------------------
