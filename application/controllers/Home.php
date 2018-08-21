a2575c160260df8663b541bec3f269c8d38c0949
abcdbar <bobbyilham44@gmail.com>
2018-08-21 12:38:27 +0700

    resolve conflict
    gak penting kok iki

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,21 +5,15 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
+		$this->load->model('home_model');
 		$this->load->library('session');
 		if($this->session->userdata('status') != "login"){
 			redirect(base_url("Login")); }       
 	}
 
+
     public function index () {
-<<<<<<< HEAD
-      //  $this ->load->view('Header');
-    	$data['err_message'] = "";
-		$data['data1'] = $this->home_model->getDataExpert();
-		$data['data2'] = $this->home_model->getDataPengetahuanTopik();
-		$data['data3'] = $this->home_model->getDataPengetahuanExpert();
-		$this->load->view('Home', $data);
-=======
-	  //  $this ->load->view('Header');
+    	
 	  	if($this->session->userdata('status') != "login"){
 			redirect(base_url("Login"));
 		}else{ 
@@ -27,10 +21,10 @@ class Home extends CI_Controller {
 			// $this->load->view('Home');
 			$data['err_message'] = "";
 			$data['data1'] = $this->home_model->getDataExpert();
+			$data['data2'] = $this->home_model->getDataPengetahuanTopik();
+			$data['data3'] = $this->home_model->getDataPengetahuanExpert();
 			$this->load->view('Home', $data);
 		}
-    	
->>>>>>> 496df43b348c83469b6d824b701b85a95d11812c
         //IKI HOME!!!
     }
 }
